92a1b2f44ae066259efa4e51895166c6e192e798
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-30 02:23:18 -0300

    ✅ Hello World

---
+++ src/main/java/tech/martindev/mwh/MyWebHomeApplication.java
@@ -0,0 +1,13 @@
+package tech.martindev.mwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/martindev/mwh/MyWebHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.martindev.mwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
