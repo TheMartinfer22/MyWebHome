c3abac085d07609299bb8986e7ac73ae47774e28
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-05 18:54:13 -0300

    ✅️ Exception AccountNotFound

---
+++ src/main/java/tech/martindev/mwh/exception/AccountNotFoundException.java
@@ -0,0 +1,8 @@
+package tech.martindev.mwh.exception;
+
+public class AccountNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public AccountNotFoundException(){
+        super();
+    }
+}
