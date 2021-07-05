9b0e1cc46f2c8ce799c1686f216cf2e8baf60408
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-05 18:54:51 -0300

    ✅️ Exception AccountPasswordWrong

---
+++ src/main/java/tech/martindev/mwh/exception/AccountPasswordWrongException.java
@@ -0,0 +1,8 @@
+package tech.martindev.mwh.exception;
+
+public class AccountPasswordWrongException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public AccountPasswordWrongException(){
+        super();
+    }
+}
