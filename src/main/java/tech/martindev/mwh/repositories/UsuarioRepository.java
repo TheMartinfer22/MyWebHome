872a12e99853ea9b19f74423ac479f5192154394
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-05 18:56:32 -0300

    ✅️ findByUsername created

--- src/main/java/tech/martindev/mwh/repositories/UsuarioRepository.java
+++ src/main/java/tech/martindev/mwh/repositories/UsuarioRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.martindev.mwh.entities.UsuarioEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+    Optional<UsuarioEntity> findByUsername(String username);
 }
