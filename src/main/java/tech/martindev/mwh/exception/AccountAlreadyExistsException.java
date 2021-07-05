5e632732954e330dd1c5414875a26cabcc6ca63d
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-05 18:53:58 -0300

    ✅️ Exception AccountAlreadyExists

---
+++ src/main/java/tech/martindev/mwh/exception/AccountAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package tech.martindev.mwh.exception;
+
+public class AccountAlreadyExistsException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public AccountAlreadyExistsException(){
+        super();
+    }
+}
