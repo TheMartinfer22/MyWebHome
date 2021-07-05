8f590982490ceeba1e8bd62fb8208d4c2307b39d
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-05 18:56:14 -0300

    ✅️ Methods CRUDE created by #2

--- src/main/java/tech/martindev/mwh/controller/UsuarioRestController.java
+++ src/main/java/tech/martindev/mwh/controller/UsuarioRestController.java
@@ -1,8 +1,11 @@
 package tech.martindev.mwh.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import tech.martindev.mwh.entities.UsuarioEntity;
 import tech.martindev.mwh.service.UsuarioService;
+
+import java.util.stream.Stream;
 
 @RestController
 public class UsuarioRestController {
@@ -13,13 +16,44 @@ public class UsuarioRestController {
         this.usuarioService = usuarioService;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createAccount(@RequestBody UsuarioEntity usuarioEntity){
-        usuarioService.createUser(usuarioEntity);
+    public void createAccount(@RequestBody UsuarioEntity usuarioEntity) throws IllegalAccessException{
+        usuarioService.createAccount(usuarioEntity);
     }
 
-    @GetMapping(path = "{id}")
-    public UsuarioEntity listAccounts(@PathVariable  Long id){
-        return usuarioService.listAccounts(id);
+    @GetMapping(path = "/get/{id}")
+    public UsuarioEntity listById(@PathVariable  Long id){
+        return usuarioService.getById(id);
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping(path = "{username}")
+    public UsuarioEntity listByUsername(@PathVariable  String username){
+        return usuarioService.getByUsername(username);
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping
+    public Stream<String> listAccounts(){
+        return usuarioService.listAccounts();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @DeleteMapping(path = "/delete/{id}")
+    public void deleteById(@PathVariable Long id){
+        usuarioService.deleteById(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(path = "/update_pass/{id}/{oldPassword}/{newPassword}")
+    public void updatePasswordById(@PathVariable Long id, @PathVariable String oldPassword, @PathVariable String newPassword){
+        usuarioService.updatePasswordById(id, oldPassword, newPassword);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(path = "/update_user/{id}/{newUsername}")
+    public void updateUsernameById(@PathVariable Long id, @PathVariable String newUsername){
+        usuarioService.updateUsernameById(id, newUsername);
     }
 }
--- src/main/java/tech/martindev/mwh/service/UsuarioService.java
+++ src/main/java/tech/martindev/mwh/service/UsuarioService.java
@@ -3,10 +3,15 @@ package tech.martindev.mwh.service;
 
 import org.springframework.stereotype.Service;
 import tech.martindev.mwh.entities.UsuarioEntity;
+import tech.martindev.mwh.exception.AccountAlreadyExistsException;
+import tech.martindev.mwh.exception.AccountNotFoundException;
+import tech.martindev.mwh.exception.AccountPasswordWrongException;
 import tech.martindev.mwh.repositories.UsuarioRepository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 @Service
 public class UsuarioService {
@@ -23,14 +28,14 @@ public class UsuarioService {
      *
      * @param usuarioEntity
      */
-    public void createAccount(UsuarioEntity usuarioEntity) throws IllegalAccessException{
+    public void createAccount(UsuarioEntity usuarioEntity) throws IllegalAccessException {
         boolean userAleartyExist = usuarioRepository
                 .findByUsername(usuarioEntity.getUsername())
                 .stream().anyMatch(e -> e.getUsername()
                         .equals(usuarioEntity.getUsername()));
 
-        if (userAleartyExist){
-            throw new IllegalAccessException("O usuário já existe.");
+        if (userAleartyExist) {
+            throw new AccountAlreadyExistsException();
         }
 
         usuarioEntity.setRegister_At(LocalDateTime.now());
@@ -39,14 +44,105 @@ public class UsuarioService {
 
     /**
      * Está recebendo a ID que está sendo passada na classe do controlador.
+     * E retornando os dados do usuário.
+     * <p>
+     * TODO Criar DTO para proteger senha de usuário (Privacity Security)
      *
      * @param id
      * @return
      */
-    public UsuarioEntity listAccounts(Long id){
+    public UsuarioEntity getById(Long id) {
         return usuarioRepository.findById(id)
                 .stream()
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("Não foi possível encontrar o usuário"));
+                .orElseThrow(AccountNotFoundException::new);
+    }
+
+    /**
+     * Está pedindo ao controller que envie uma string que corresponde
+     * ao nome de usuário e então irá procurar
+     * no database se aquele usuário existe, caso não existir irá
+     * retornar uma exceção pernsonalizada.
+     * <p>
+     * Irá retornar a conta completa do usuário e irá pegar o
+     * primeiro resultado pois, está sendo validado.
+     *
+     * @param username
+     * @return
+     */
+    public UsuarioEntity getByUsername(String username) {
+        return usuarioRepository.findByUsername(username)
+                .stream()
+                .findFirst()
+                .orElseThrow(AccountNotFoundException::new);
+    }
+
+    /**
+     * Está retornando todos usuários registrados mas,
+     * apenas o nome de usuário.
+     *
+     * @return
+     */
+    public Stream<String> listAccounts() {
+        return usuarioRepository.findAll().stream().map(UsuarioEntity::getUsername);
+    }
+
+    /**
+     * Irá receber a id que está sendo passado no controller,
+     * então irá verificar se a ID existe, caso existir irá deletar o usuário com
+     * a id que foi passada, caso contrário irá lançar uma exceção personalizada.
+     *
+     * @param id
+     */
+    @Transactional
+    public void deleteById(Long id) {
+        if (usuarioRepository.findById(id).isPresent()) {
+            usuarioRepository.deleteById(id);
+        } else {
+            throw new AccountNotFoundException();
+        }
+    }
+
+    /**
+     * Irá receber o  parâmetro Path que foi passado no controlador
+     * e irá verificar se a conta existe,
+     * também irá ser responsável por ver se a senha que foi passada é a antiga do usuário
+     * para assim garantir segurança.
+     *
+     * @param id
+     * @param newPassword
+     */
+    @Transactional
+    public void updatePasswordById(Long id, String oldPassword, String newPassword) {
+        if (usuarioRepository.findById(id).isEmpty()){
+            throw new AccountNotFoundException();
+        }
+
+        usuarioRepository.findById(id).ifPresent(usuarioEntity -> {
+            if (usuarioEntity.getPassword().equals(oldPassword)) {
+                usuarioRepository.findById(id).ifPresent(username -> {
+                    username.setPassword(newPassword);
+                });
+            } else {
+                throw new AccountPasswordWrongException();
+            }
+        });
+    }
+
+    @Transactional
+    public void updateUsernameById(Long id, String newUsername) {
+        if (usuarioRepository.findById(id).isEmpty()){
+            throw new AccountNotFoundException();
+        }
+
+        usuarioRepository.findById(id).ifPresent(usuarioEntity -> {
+            if (usuarioRepository.findByUsername(newUsername).isPresent()){
+                throw new AccountAlreadyExistsException();
+            }
+            if (usuarioEntity.getUsername().equals(newUsername)){
+                throw new AccountAlreadyExistsException();
+            }
+            usuarioEntity.setUsername(newUsername);
+        });
     }
 }
